cb9425db86542fbc630de9203857d75fbf3bbad4
Balazs Nagy <balazs_nagy-budd@mckinsey.com>
2023-03-02 13:39:55 +0100

    Modify env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. {process.env.TEST}
+          Edit <code>src/App.js</code> and save to reload. Z1 {process.env.TEST}
         </p>
         <a
           className="App-link"
